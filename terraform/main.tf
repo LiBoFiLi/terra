d65b9448ff7781a083b7f4c752b9c4b12c306db6
Li_Bo <dimon240504@gmail.com>
2024-06-30 00:27:31 +0300

    add main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  s3_force_path_style         = true
+  endpoints {
+    s3      = "http://localhost:4566"
+    lambda  = "http://localhost:4566"
+  }
+}
